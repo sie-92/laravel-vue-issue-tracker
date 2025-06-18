eb55780a84eb7b6022df926c98b333a4b2b17610
Imed Eddine SAKRAOUI <sie-92@users.noreply.github.com>
2025-06-18 14:48:54 +0100

    Create axios.js

---
+++ frontend/src/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:8000/api',
+  withCredentials: true,
+  headers: {
+    Accept: 'application/json',
+  },
+});
+
+export default api;
