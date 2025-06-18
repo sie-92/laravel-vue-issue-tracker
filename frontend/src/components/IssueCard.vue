5aec449bf210cc0489db0a888482d485d5c0490e
Imed Eddine SAKRAOUI <sie-92@users.noreply.github.com>
2025-06-18 14:51:35 +0100

    Create IssueCard.vue

---
+++ frontend/src/components/IssueCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>{{ issue.title }}</h3>
+    <p>{{ issue.description }}</p>
+    <p>Status: {{ issue.status }}</p>
+    <button @click="remove">Delete</button>
+  </div>
+</template>
+
+<script setup>
+import api from '../axios';
+const props = defineProps(['issue']);
+const emit = defineEmits(['refresh']);
+
+const remove = async () => {
+  await api.delete(`/issues/${props.issue.id}`);
+  emit('refresh');
+};
+</script>
